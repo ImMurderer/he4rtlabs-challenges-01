04dac9993f6762bbc52f1c754538bd712eec6675
Immurderer <joaopedro0128@hotmail.com>
2020-02-22 17:28:45 -0300

    almost done

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const formPage = document.querySelector("form");
+const resultsPage = document.querySelector("#results");
+const formatCurrency = (e) => {
+    e.target.value = Number(e.target.value).toFixed(2);
+}
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const entrys = {};
+    document.querySelectorAll("input").forEach(v => {
+        entrys[v.name] = v.value;
+        v.value = "";
+    });
+    const { valorProjeto, diasEfetivos, horasDiarias, diasFerias } = entrys;
+    const valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ((diasFerias * diasEfetivos * horasDiarias));
+    document.querySelector("#earnings").innerHTML = `R$ ${valorHora.toFixed(2)}`;
+    resultsPage.style.zIndex = 10;
+}
+formPage.addEventListener("submit", handleSubmit);
+document.querySelector("input[name='valorProjeto']").addEventListener("change", formatCurrency);
+document.querySelector("#reset").addEventListener("click", (e) => {
+    resultsPage.style.zIndex = -10;
+})
